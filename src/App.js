8b82b7c329e4e5aab7cf33f2ef82a0a67dce8ada
Sebastien <delsebastienjames@gmail.com>
2021-05-06 13:48:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./App.css";
 
+import Home from "./containers/Home";
+import Offer from "./containers/Offer";
+
 const App = () => {
   return <div className="container">Hello World</div>;
 };
