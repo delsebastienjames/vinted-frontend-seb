c57227e3df137c9e2fea9cb109a6ac1b5c64f007
Sebastien <delsebastienjames@gmail.com>
2021-05-06 16:02:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; // BrowserRouter as Router permet de renommer BrowserRouter en (Route)
