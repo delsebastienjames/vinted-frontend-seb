d9a12337a99575636f4cc33b6918b5171aa611d7
Sebastien <delsebastienjames@gmail.com>
2021-05-06 15:50:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import "./App.css";
 
-import Home from "./containers/Home";
-import Offer from "./containers/Offer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; // BrowserRouter as Router permet de renommer BrowserRouter en (Route)
+
+import Home from "./containers/Home"; // Composant Home
+import Offer from "./containers/Offer"; // Composant Offer
 
 const App = () => {
-  return <div className="container">Hello World</div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/offer/:id">
+            <Offer />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
