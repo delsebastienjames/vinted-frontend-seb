35f909c3c73080db7112097bf5d24905453c81dd
Sebastien <delsebastienjames@gmail.com>
2021-05-09 06:16:52 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,24 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
-const Header = () => {
-  return <div className="header">Vinted</div>;
+const Header = ({ userToken, setUser }) => {
+  return (
+    <div className="header">
+      Vinted
+      {userToken ? (
+        <button className="connecter" onClick={() => setUser(null)}>
+          Se déconnecter
+        </button>
+      ) : (
+        <>
+          <Link className="inscrire" to="/signup">
+            S'inscrire
+          </Link>
+          <Link className="connecter" to="/login">
+            Se connecter
+          </Link>
+        </>
+      )}
+    </div>
+  );
 };
 export default Header;
