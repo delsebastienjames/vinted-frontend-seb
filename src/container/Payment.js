ab09dd754933a0c97cd0b8304bac8de7fb8f9592
Sebastien <delsebastienjames@gmail.com>
2021-05-11 22:38:38 +0200

    Create Payment.js

---
+++ src/container/Payment.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+const Payment = () => {
+  const location = useLocation();
+  const { title } = location.state;
+
+  return <span>{title}</span>;
+};
+
+export default Payment;
