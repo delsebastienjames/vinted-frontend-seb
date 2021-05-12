5f524da6886d546b49c3c2aa7174352be5efb96f
Sebastien <delsebastienjames@gmail.com>
2021-05-12 19:50:22 +0200

    Update Payment.js

--- src/container/Payment.js
+++ src/container/Payment.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "../components/CheckoutForm";
+
+const stripePromise = loadStripe(
+  "pk_test_51Ipz9pJ4chTsmqwAz7TIRCUoaq1xWpQCQ5ZuROWpRervhaiFMUUWm3KhnAc3DkwG0iEJyCnwRYyFfztXcH962SaW00sGzUZnij"
+);
 
 const Payment = () => {
   const location = useLocation();
-  const { title } = location.state;
-
-  return <span>{title}</span>;
+  return (
+    <Elements stripe={stripePromise}>
+      <CheckoutForm data={location.state.data} />
+    </Elements>
+  );
 };
 
 export default Payment;
