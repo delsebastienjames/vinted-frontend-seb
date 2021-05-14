08d64247f7454c283185d71d615bfd1315aa0510
Sebastien <delsebastienjames@gmail.com>
2021-05-14 12:54:40 +0200

    Update Payment.js

--- src/container/Payment.js
+++ src/container/Payment.js
@@ -4,11 +4,11 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "../components/CheckoutForm";
 
-const stripePromise = loadStripe(
-  "pk_test_51Ipz9pJ4chTsmqwAz7TIRCUoaq1xWpQCQ5ZuROWpRervhaiFMUUWm3KhnAc3DkwG0iEJyCnwRYyFfztXcH962SaW00sGzUZnij"
-);
-
 const Payment = () => {
+  const stripePromise = loadStripe(
+    "pk_test_51Ipz9pJ4chTsmqwAz7TIRCUoaq1xWpQCQ5ZuROWpRervhaiFMUUWm3KhnAc3DkwG0iEJyCnwRYyFfztXcH962SaW00sGzUZnij"
+  );
+
   const location = useLocation();
   return (
     <Elements stripe={stripePromise}>
