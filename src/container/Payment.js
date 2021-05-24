0ca41c77292fda70103fc301a514ec7e510bd24d
Sebastien <delsebastienjames@gmail.com>
2021-05-24 13:45:53 +0200

    Update Payment.js

--- src/container/Payment.js
+++ src/container/Payment.js
@@ -4,11 +4,12 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "../components/CheckoutForm";
 
-// Clé publique à envoyer à l'API Stripe
-const stripePromise = loadStripe(
-  "pk_test_51Ipz9pJ4chTsmqwAz7TIRCUoaq1xWpQCQ5ZuROWpRervhaiFMUUWm3KhnAc3DkwG0iEJyCnwRYyFfztXcH962SaW00sGzUZnij"
-);
 const Payment = () => {
+  // Clé publique à envoyer à l'API Stripe
+  const stripePromise = loadStripe(
+    "pk_test_51Ipz9pJ4chTsmqwAz7TIRCUoaq1xWpQCQ5ZuROWpRervhaiFMUUWm3KhnAc3DkwG0iEJyCnwRYyFfztXcH962SaW00sGzUZnij"
+  );
+
   const location = useLocation();
   return (
     // Tout ce qui est contenu dans Elements a accès à l'API Stripe
