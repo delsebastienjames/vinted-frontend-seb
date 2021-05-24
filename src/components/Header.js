4457e2f6cde30f48dfce75e12005a90c05401f0f
Sebastien <delsebastienjames@gmail.com>
2021-05-24 06:30:46 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-// import { Link, Redirect } from "react-router-dom";
+//import { Link, Redirect } from "react-router-dom";
 
 const Header = ({ userToken, setUser }) => {
   return (
